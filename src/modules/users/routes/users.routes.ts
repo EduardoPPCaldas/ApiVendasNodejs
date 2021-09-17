7fcc8362ebd327a0e85afeb3bd12ed59f7879175
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2021-09-17 01:13:34 -0300

    Add: Multer upload of files

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { celebrate, Joi, Segments } from "celebrate";
+import multer from "multer";
+import uploadConfig from "@config/upload";
 import { UsersController } from "../controllers/UsersController";
 import { isAuthenticated } from "@shared/http/middlewares/isAuthenticated";
+import { UserAvatarController } from "../controllers/UserAvatarController";
 
 
 const usersRouter = Router()
 
 const usersController = new UsersController()
+const usersAvatarController = new UserAvatarController()
+
+const upload = multer(uploadConfig)
 
 usersRouter.get("/", isAuthenticated ,usersController.index)
 
@@ -31,6 +37,13 @@ usersRouter.delete(
   }),
   isAuthenticated,
   usersController.delete
+)
+
+usersRouter.patch(
+  "/avatar",
+  isAuthenticated,
+  upload.single("avatar"),
+  usersAvatarController.update
 )
 
 export default usersRouter
