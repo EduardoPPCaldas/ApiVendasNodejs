dc3963b59b93d32dcdf79cd999bbce8dc2daf106
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2021-09-23 13:03:45 -0300

    Add: Cache with Redis

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -2,6 +2,8 @@ import AppError from "@shared/errors/AppError";
 import { getCustomRepository } from "typeorm";
 import {ProductsRepository} from "../typeorm/repositories/ProductsRepository"
 import Product from "../typeorm/entities/Product"
+import { RedisCache } from "@shared/cache/RedisCache";
+
 
 interface IRequest {
   name: string
@@ -17,11 +19,15 @@ export class CreateProductService {
       throw new AppError("There is already one product with this name")
     }
 
+    const redisCache = new RedisCache()
+
     const product = productsRepository.create({
       name,
       price,
       quantity
     })
+
+    await redisCache.invalidate("api-vendas-PRODUCT_LIST")
 
     await productsRepository.save(product)
 
--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,6 +1,7 @@
 import { getCustomRepository } from "typeorm";
 import {ProductsRepository} from "../typeorm/repositories/ProductsRepository"
 import AppError from "@shared/errors/AppError";
+import { RedisCache } from "@shared/cache/RedisCache";
 
 interface IRequest {
   id: string
@@ -15,6 +16,10 @@ export class DeleteProductService {
     if(!product){
       throw new AppError("Product not found")
     }
+
+    const redisCache = new RedisCache()
+
+    await redisCache.invalidate("api-vendas-PRODUCT_LIST")
 
     await productsRepository.remove(product)
 
